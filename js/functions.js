8b0615e8f59c290526ae632d129757b0af56446d
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-08-23 17:51:42 +0200

    Bonus Completed. Adjust some style, added comments.

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,3 @@
-/**
- * Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-# MILESTONE 1
-Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-
-#MILESTONE 2
-Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play,
-vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-
-#MILESTONE 3
-In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
-
-#MILESTONE 4
-Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
-
-# BONUS
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-Note:
-- questo bonus richiederà un evento diverso dal 'click'
-- questo bonus richiederà una riflessione extra per quel che riguarda il calcolo della larghezza delle celle :occhiolino:
- */
-
-
 // createCell
 
 // Funzione per generare un nodo, in questo caso un div
@@ -36,11 +5,21 @@ Note:
  * 
  * @param {content} any
  * Parametro che sarà inserito come contenuto del nodo
- * @returns div class='cell'
+ * @param {difficulty} string
+ * parametro che mediante una stringa fornisce la difficoltà selezionata, utile per ridimensionare la griglia propriamente
+ * @returns div class='cell', + eventuali classi aggiuntive
  */
-function createCell(content) {
+
+function createCell(content, difficulty) {
     const cell = document.createElement('div');       // Creo l'elemento, in questo caso sarà sempre un div, perche è scritto in hardcode, e non come parametro
-    cell.className = 'cell';                          // Setto la classe 'cell', dato che non ne aveva
+    cell.className = 'cell';                          // Setto la classe a 'cell', dato che non ne aveva
+    switch (difficulty) {                             // il parametro difficulty indica la difficoltà con cui vuole giocare l'utente, maggiore è minore saranno le celle
+        case 'hard':                                  // aggiungo classe per controllo dimensioni
+            cell.classList.add('hardDif');           
+            break;    
+        case 'medium':
+            cell.classList.add('medDif');             // aggiungo classe per controllo dimensioni
+    }
     cell.append(content);                             // 'inserisco' il parametro passato alla funzione nell'elemento
     return cell;                                      // ritorno il nodo creato a chi mi invoca
 };
--- js/script.js
+++ js/script.js
@@ -14,27 +14,48 @@
 
 const grid = document.getElementById('grid');
 const playBtn = document.querySelector('button');
+const difficultyField = document.querySelector('select');
 const form = document.querySelector('form');
 
 // Variabili note
 
-const rows = 10;
-const cols = 10;
-const totCells = rows * cols;
+let rows;
+let cols;
+let totCells;
 
 
 // Eventi dinamici
 
 // Al submit del form, genero una griglia 10 x 10 di nodi div, contententi i numeri in ordine da 1 a 100
 form.addEventListener('submit', function(event) {
-    event.preventDefault();                                         // prevengo il refresh della pagina
-    
-    for ( let i = 0; i < totCells; i++) {                           // giro per quante 'celle' voglio nella griglia, variabili di partenza a noi note
-        const cell = createCell(i + 1);                             // invoco la funzione per generare le celle di nodi che voglio. Il Parametro è la variabile di controllo del for in cui è inserita la funzione, aumentato di 1 per stamapre i numeri da 1 a 100
+    event.preventDefault();                              // prevengo il refresh della pagina
+
+    const difficulty = difficultyField.value;            // recupero il valore della select, contenente la difficoltà impostata dall'utente
+
+    switch (difficulty) {                                // controllo il valore della difficoltà selezionata, per impostare il numero di righe e colonne          
+        case 'hard':
+            rows = 7;
+            cols = 7;            
+            break;
+        case 'medium':
+            rows = 9;
+            cols = 9;
+            break;
+        default:                                        // il valore 'easy' di difficoltà diventa il default del gioco
+            rows = 10;
+            cols = 10;
+            break;
+    }
+
+    totCells = rows * cols;                                         // Solo ora che ho decretato la difficoltà con cui il giocatore vuole cimentarsi, posso calcolare il tot delle celle della griglia
+
+
+    for (let i = 0; i < totCells; i++) {                            // giro per quante 'celle' voglio nella griglia
+        const cell = createCell(i + 1, difficulty);                 // invoco la funzione per generare le celle di nodi che voglio. Il primo Parametro è la variabile di controllo del for in cui è inserita la funzione, aumentato di 1 per stamapre i numeri da 1 a 100, il secondo invece è la difficoltà del gioco
                                              
         cell.addEventListener('click', function() {                 // per ogni cella creata aggiungo un event listener, che reagirà al click
             console.log(this.innerText, ': è il contenuto della cella che hai cliccato'); // stampa in console del contenuto testuale del nodo cliccato, grazie a this
-            this.classList.add('clicked');                          // sempre al click sulla cella, aggiungo la classe clicked, per cambiarne il colore di background in pagina, si può migliorare con toggle al posto di add
+            this.classList.toggle('clicked');                       // sempre al click sulla cella, aggiungo la classe clicked, per cambiarne il colore di background in pagina
         });
         grid.appendChild(cell);                                     // aggiungo alla griglia in pagina il nodo appena creato
     }
